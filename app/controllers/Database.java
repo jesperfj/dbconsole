62f638a80977e553a302dc83fa7e712ed65e678d
Jesper Joergensen <jesper@heroku.com>
2012-01-04 10:52:37 -0800

    Removed debug output and removed username from frontdoor login URL. It's not needed

--- app/controllers/Database.java
+++ app/controllers/Database.java
@@ -18,10 +18,8 @@ public class Database extends ParentController {
     public static void weblogin(String id) {
 
     	DatabaseLink d = coredb.query("SELECT username__c, token__c FROM database__c WHERE id='"+id+"'", DatabaseLink.class).getRecords().get(0);
-    	
     	ApiSession s = Auth.refreshOauthTokenFlow(CONFIG, d.getToken());
-    	System.out.println("Exchanged refresh token for access token: "+s.getAccessToken());
-    	redirect(s.getApiEndpoint()+"/secur/frontdoor.jsp?un="+d.getUsername()+"&sid="+s.getAccessToken()+"&startURL=/home/home.jsp");
+    	redirect(s.getApiEndpoint()+"/secur/frontdoor.jsp?sid="+s.getAccessToken()+"&startURL=/home/home.jsp");
     }
     
     public static void delete(String id) {
